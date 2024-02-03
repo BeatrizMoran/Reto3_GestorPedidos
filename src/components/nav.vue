b706891248a76998edb70553ae87017ec95efc27
beatriz <beatriz.moran@ikasle.egibide.org>
2024-02-03 19:08:25 +0100

    estilo nav

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -34,34 +34,37 @@
           <router-link to="/cart" class="nav-link" active-class="active">Cesta compra</router-link>
         </li>
         <li class="nav-item col-5" v-if="!clienteEnLocalStorage">
-            <button type="button" class="btn btn-outline-success mx-3" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-              Entrar
-            </button>
+          <button type="button" class="btn btn-outline-success mx-3" data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop">
+            Entrar
+          </button>
 
-            <!-- Modal -->
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-              aria-labelledby="staticBackdropLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    <input class="form-control form-control-sm" type="search" placeholder="Insertar Código..."
-                      aria-label="Codificacion" @keypress.enter="checkCodigo" v-model="codigoCLiente" enabled />
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                    <button type="button" class="btn btn-primary" @click="checkCodigo">Entrar</button>
-                  </div>
+          <!-- Modal -->
+          <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <input class="form-control form-control-sm" type="search" placeholder="Insertar Código..."
+                    aria-label="Codificacion" @keypress.enter="checkCodigo" v-model="codigoCLiente" enabled />
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                  <button type="button" class="btn btn-primary" @click="checkCodigo">Entrar</button>
                 </div>
               </div>
             </div>
+          </div>
 
         </li>
         <li class="nav-item" v-if="clienteEnLocalStorage">
-          <a class="nav-link" active-class="active" @click="cerrarSesion">Cerrar sesion</a>
+          <button type="button" class="btn btn-outline-danger mx-3" active-class="active" @click="cerrarSesion">
+            Cerrar sesion
+          </button>
         </li>
       </ul>
     </div>
