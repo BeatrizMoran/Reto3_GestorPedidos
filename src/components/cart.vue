5e3e233b0f4fab1e61bddd49d041b3891b6d6e6c
JavierJamaicaCast <javiernicolasjamaica@gmail.com>
2024-02-04 21:10:31 +0100

    Tratando de hacer el responsive

--- src/components/cart.vue
+++ src/components/cart.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <div class="col-8">
+    <div class="col col-md-8">
       <p class="p-3 rounded-3 carrito-color">
         <img
           src="../assets/images/carrito.png"
